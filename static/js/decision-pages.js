2280ca5303e472d93d82d6beb3dd9a3adadb2edd
Radot1 <bzoumboulis@yahoo.co.uk>
2026-03-29 01:06:12 +0200

    update

---
+++ static/js/decision-pages.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    var groups = document.querySelectorAll('[data-faq-accordion]');
+    if (!groups.length) {
+        return;
+    }
+
+    groups.forEach(function (group) {
+        var items = group.querySelectorAll('details');
+        items.forEach(function (item) {
+            item.addEventListener('toggle', function () {
+                if (!item.open) {
+                    return;
+                }
+                items.forEach(function (other) {
+                    if (other !== item) {
+                        other.open = false;
+                    }
+                });
+            });
+        });
+    });
+})();
